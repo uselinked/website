52b6db7b9f085f0317d7b0d68cd93c812823fbfb
lostdesign <info@lost.design>
2021-06-04 17:25:40 +0200

    ♻️  add github link

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <section>
-    <header><h1>linked</h1></header>
+  <section class="mt-12">
+    <header>
+      <h1 class="text-4xl">linked</h1>
+      <div>
+        site is work in progress, check
+        <a class="underline" href="https://github.com/lostdesign/linked"
+          >github</a
+        >
+        for now
+      </div>
+    </header>
   </section>
 </template>

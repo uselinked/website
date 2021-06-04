462708e7a5329eeed81b1ec276b679c18fc75223
lostdesign <info@lost.design>
2021-06-04 14:50:05 +0200

    🎨 fix lint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,17 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true,
+    node: true
   },
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: 'babel-eslint'
   },
   extends: [
     '@nuxtjs',
     'plugin:prettier/recommended',
-    'plugin:nuxt/recommended',
+    'plugin:nuxt/recommended'
   ],
   plugins: [],
   // add your custom rules here
-  rules: {},
+  rules: {}
 }

aa6f8609234f48c6069bc28d6a09635cc90853ae
lostdesign <info@lost.design>
2021-07-08 12:59:30 +0200

    ♻️ add some text if there is nothing for that day

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   darkMode: 'class',
+  purge: {
+    enabled: true,
+    content: [
+      './pages/**/*.vue',
+      './layouts/**/*.vue',
+      './components/**/*.vue',
+      './assets/**/*.svg',
+      './assets/**/*.css'
+    ]
+  },
   theme: {},
   variants: {},
   plugins: []

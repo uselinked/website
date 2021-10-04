092d27a5fa8f5d321a846e8efa9200d513180f88
wellá <info@lost.design>
2021-10-04 17:35:37 +0200

    ♻️  remove code from discord section

--- components/discord-section.vue
+++ components/discord-section.vue
@@ -13,87 +13,33 @@
           </p>
         </div>
         <div class="mt-8 lg:mt-0 lg:ml-8">
-          <form class="sm:flex" @submit.prevent="subscribe">
-            <div
-              class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3 sm:flex-shrink-0"
+          <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3 sm:flex-shrink-0">
+            <a
+              href="https://discord.gg/uNjJzZvccr"
+              target="_blank"
+              class="
+                w-full
+                flex
+                items-center
+                justify-center
+                py-3
+                px-5
+                border border-transparent
+                text-base
+                font-medium
+                rounded-md
+                text-white
+                bg-red-500
+                hover:bg-red-400
+                focus:outline-none
+                focus:ring-2 focus:ring-offset-2 focus:ring-red-500
+              "
             >
-              <a
-                href="https://discord.gg/uNjJzZvccr"
-                target="_blank"
-                class="
-                  w-full
-                  flex
-                  items-center
-                  justify-center
-                  py-3
-                  px-5
-                  border border-transparent
-                  text-base
-                  font-medium
-                  rounded-md
-                  text-white
-                  bg-red-500
-                  hover:bg-red-400
-                  focus:outline-none
-                  focus:ring-2 focus:ring-offset-2 focus:ring-red-500
-                "
-              >
-                {{ $t('discord.join') }}
-              </a>
-            </div>
-          </form>
+              {{ $t('discord.join') }}
+            </a>
+          </div>
         </div>
       </div>
     </div>
   </section>
 </template>
-
-<script>
-import axios from 'axios'
-
-export default {
-  data() {
-    return {
-      email: null,
-      error: null,
-      success: false,
-      loading: false
-    }
-  },
-
-  methods: {
-    subscribe() {
-      if (this.loading || !this.email) {
-        return
-      }
-
-      this.loading = true
-
-      const options = {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }
-
-      axios
-        .post(
-          'https://news.lost.services/mailcoach/subscribe/0455ff01-00ed-4694-a516-be3be919f23e',
-          { email: this.email },
-          options
-        )
-        .then(() => {
-          this.email = null
-          this.success = true
-          this.error = null
-          this.loading = false
-        })
-        .catch((error) => {
-          this.success = false
-          this.error =
-            error?.reponse?.data?.errors?.email?.[0] ||
-            error?.response?.data?.message
-        })
-    }
-  }
-}
-</script>

e5dc00e5d194f2a3a55b1565bb43a370de221d5c
wellá <info@lost.design>
2022-02-24 09:57:47 +0100

    🚧 add release page

---
+++ pages/download/_version.vue
@@ -0,0 +1,21 @@
+<template>
+  <content-wrapper>
+    {{ $route.params.version }}
+    <pre>
+      {{ version.assets }}
+    </pre>
+  </content-wrapper>
+</template>
+
+<script>
+export default {
+  name: 'DownloadPage',
+  async asyncData({ params, redirect }) {
+    const version = await fetch(
+      'https://api.github.com/repos/lostdesign/linked/releases/tags/v' + params.version
+    ).then((res) => res.json())
+
+    return {version}
+  }
+}
+</script>

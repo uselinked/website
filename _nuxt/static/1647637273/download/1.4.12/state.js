c6b7128abe044c42c0c19625831bf4323f02e38e
lostdesign <lostdesign@users.noreply.github.com>
2022-03-18 21:01:20 +0000

    deploy: b666bb5631a5b9149b728c1dbada1ad75fdd0234

---
+++ _nuxt/static/1647637273/download/1.4.12/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1647637273",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdownload\u002F1.4.12",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c57cbe4b"}}}}(null));

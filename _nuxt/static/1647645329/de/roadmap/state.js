116b5cc033e869ede35674a5169595d5d655b23c
lostdesign <lostdesign@users.noreply.github.com>
2022-03-19 00:13:46 +0000

    deploy: 07ce8d1050681ce216e6872a48e901b6acf1bee1

--- _nuxt/static/1647645329/de/roadmap/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1647645329",layout:"default",error:a,serverRendered:true,routePath:"\u002Fde\u002Froadmap",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a5650560"}}}}(null));
--- _nuxt/static/1647645329/download/1.4.12/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1647645329",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdownload\u002F1.4.12",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a5650560"}}}}(null));

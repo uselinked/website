e4835837858aee844a7fa1d148eef7df66a25edb
lostdesign <lostdesign@users.noreply.github.com>
2022-03-19 23:32:18 +0000

    deploy: bd8e4d3ea211ec85af51a1acad4218461e86d927

---
+++ _nuxt/static/1647732731/download/0.0.1/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/download/0.0.1", {data:[{release:null,error:{error:true,reset:"in 59 minutes",message:"You've exceeded the request count for the Github API, please retry in 59 minutes."},fallbackUrl:"https:\u002F\u002Fgithub.com\u002Flostdesign\u002Flinked\u002Freleases\u002Ftags\u002Fv0.0.1"}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1647732731/download/1.3.10/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1647732731",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdownload\u002F1.3.10",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"779adfc5"}}}}(null));

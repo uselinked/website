51adfeedd3b7d58d3e579ddfd89c6388a813d3d5
wellá <info@lost.design>
2022-03-20 15:44:26 +0100

    🌐 translate testimonials

--- components/section/testimonial.vue
+++ components/section/testimonial.vue
@@ -15,7 +15,7 @@
         <div class="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <blockquote class="mt-10 mb-6">
             <div class="relative max-w-3xl mx-auto text-center text-xl leading-9 font-medium text-gray-900 relative">
-              <p>&ldquo;{{ testimonial.text }}&rdquo;</p>
+              <p>&ldquo;{{ $t(testimonial.translationKey) }}&rdquo;</p>
             </div>
             <footer class="mt-8">
               <div class="md:flex md:items-center md:justify-center">
@@ -56,25 +56,25 @@ export default {
         {
           name: 'Rama Adi Nugraha',
           twitter: 'https://twitter.com/its_ramaadi',
-          text: 'I love how simple and straightforward linked is. The moment you open the app, you can just start jotting down notes with no fuss. Journaling has never been easier!',
+          translationKey: 'rama',
           image: 'https://pbs.twimg.com/profile_images/1312050517078470656/Ln2AgW5k_400x400.png'
         },
         {
           name: 'Richard',
           twitter: 'https://twitter.com/richardevcom',
-          text: 'Finally no more countless and forever lost .txt files on my workstation. Linked is perfect solution for everything in between countless todo notes and digital journals.',
+          translationKey: 'richard',
           image: 'https://pbs.twimg.com/profile_images/1416475956022325258/4yonnJYj_400x400.jpg'
         },
         {
           name: 'Sebastian',
           twitter: 'https://twitter.com/T0TProduction',
-          text: 'I use linked at work daily! It\'s just so convenient for taking quick notes. What I especially like is that everything is stored locally - No need to think about data confidentiality.',
+          translationKey: 'sebastian',
           image: 'https://raw.githubusercontent.com/T0TProduction/diluz.io/deploy/assets/images/profile/sebastian/pp.png'
         },
         {
           name: 'Josiah Rose',
           twitter: 'https://twitter.com/_josiahrose',
-          text: 'I use linked because it helps me effectively manage, track and organize my daily events. It\'s special because it has a minimalist design while maintaining it\'s beneficial features and time management.',
+          translationKey: 'josiah',
           image: 'https://pbs.twimg.com/profile_images/1495911592420155395/y6ZU7Mca_400x400.jpg'
         }
       ]
@@ -94,3 +94,20 @@ export default {
   }
 }
 </script>
+
+<i18n>
+{
+  "en": {
+    "rama": "I love how simple and straightforward linked is. The moment you open the app, you can just start jotting down notes with no fuss. Journaling has never been easier!",
+    "richard": "Finally no more countless and forever lost .txt files on my workstation. Linked is perfect solution for everything in between countless todo notes and digital journals.",
+    "sebastian": "I use linked at work daily! It's just so convenient for taking quick notes. What I especially like is that everything is stored locally - No need to think about data confidentiality.",
+    "josiah": "I use linked because it helps me effectively manage, track and organize my daily events. It's special because it has a minimalist design while maintaining it's beneficial features and time management."
+  },
+  "de": {
+    "rama": "Ich finde es toll, wie einfach und unkompliziert linked ist. In dem Moment, in dem man die App öffnet, kann man ohne viel Aufwand mit dem Notieren beginnen. Journaling war noch nie so einfach!",
+    "richard": "Endlich keine unzähligen und für immer verlorenen .txt-Dateien mehr auf meinem Computer. linked ist die perfekte Lösung für alles, was zwischen unzähligen ToDo-Notizen und digitalen Journalen liegt.",
+    "sebastian": "Ich verwende linked täglich bei der Arbeit! Es ist einfach so praktisch, um schnelle Notizen zu machen. Besonders gut gefällt mir, dass alles lokal gespeichert wird - man muss sich keine Gedanken über die Vertraulichkeit der Daten machen.",
+    "josiah": "Ich verwende linked, weil es mir hilft, meine täglichen Ereignisse effektiv zu verwalten, zu verfolgen und zu organisieren. Es ist etwas Besonderes, weil es ein minimalistisches Design hat, während es seine nützlichen Funktionen und Zeitmanagement beibehält."
+  }
+}
+</i18n>

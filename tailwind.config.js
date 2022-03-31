37d9b67817c263ecbb6126135c500ae0efee149e
wellá <info@lost.design>
2022-03-31 21:36:44 +0200

    ♻️ remove comment

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: ['./**/*.vue'],
-  darkMode: 'media', // or 'media' or 'class'
+  darkMode: 'media',
   theme: {
     extend: {
       colors: {

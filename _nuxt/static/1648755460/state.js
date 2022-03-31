29283964ac5d90db61d267b229cc2de653f680bc
lostdesign <lostdesign@users.noreply.github.com>
2022-03-31 19:42:37 +0000

    deploy: 64d8ac20e5ea5d3dcbd0734badb52829476f4b22

--- _nuxt/static/1648755460/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1648755460",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8dda3cfa"}}}}(null,"\u002F"));
---
+++ _nuxt/static/1648755750/de/license/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1648755750",layout:"default",error:a,serverRendered:true,routePath:"\u002Fde\u002Flicense",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"310525a9"}}}}(null));

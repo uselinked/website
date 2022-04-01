71ceee8b45c737c8116d878e39d52550cad75568
lostdesign <lostdesign@users.noreply.github.com>
2022-04-01 16:57:07 +0000

    deploy: 847958b473893283e4a53d18c8e38413658c4da8

--- _nuxt/static/1648757929/de/donate/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/de/donate", {data:[{}],fetch:{},mutations:void 0});
--- _nuxt/static/1648757929/download/1.1.3/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1648757929",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdownload\u002F1.1.3",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"fcc21988"}}}}(null));

7ada50d5bc9f0e7bd2cff9c349946e1d8707f188
wellá <info@lost.design>
2022-04-03 16:24:32 +0200

    ✨ translate faq

--- components/section/faq.vue
+++ components/section/faq.vue
@@ -5,23 +5,10 @@
       <i18n path="description" tag="h2" class="mt-5 text-xl text-gray-500">
         <a :href="$t('mailto')" class="external-link font-medium text-linked-700 hover:text-linked-500">{{ $t('email') }}</a>
       </i18n>
-      <!--    <p class="mt-4 text-base text-gray-500">-->
-      <!--      Questions. Frequently asked ones. Plus our answers. That's how FAQs work. If you can't find what you're looking for, you can always-->
-      <!--      <a href="#" class="font-medium text-linked-700 hover:text-linked-500">send us an email</a>-->
-      <!--      with your enquiry.-->
-      <!--    </p>-->
-      <!--    <dl class="mt-12 grid grid-cols-1 gap-y-10 sm:mt-16 md:grid-cols-2 md:gap-x-6 lg:grid-cols-3">-->
-      <!--      <template v-for="(faq, index) in faqs">-->
-      <!--        <div>-->
-      <!--          <dt class="text-xl font-medium text-gray-900">{{ faq.question }}</dt>-->
-      <!--          <dd class="mt-3 text-base leading-6 tracking-wide text-gray-700">{{ faq.answer }}</dd>-->
-      <!--        </div>-->
-      <!--      </template>-->
-      <!--    </dl>-->
       <dl class="mt-12 space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-12 lg:grid-cols-3">
         <div v-for="(faq, index) in faqs" :key="index">
-          <dt class="text-lg leading-6 font-medium text-gray-900">{{ faq.question }}</dt>
-          <dd class="mt-2 text-base text-gray-500">{{ faq.answer }}</dd>
+          <dt class="text-lg leading-6 font-medium text-gray-900">{{ $t(`questions.${faq}.q`) }}</dt>
+          <dd class="mt-2 text-base text-gray-500">{{ $t(`questions.${faq}.a`) }}</dd>
         </div>
       </dl>
     </content-wrapper>
@@ -32,16 +19,7 @@
 export default {
   data() {
     return {
-      faqs: [
-        {
-          question: 'Why is linked free?',
-          answer: 'Don\'t worry, you are not paying with your personal data. The app is free and opensource since I like opensource software myself. The idea behind this that you have full control over your data and future of linked. If you want to support linked financially, you can consider sponsoring me HERE.'
-        },
-        {
-          question: 'How can I exit the Codeblock',
-          answer: 'Press Command/Ctrl + Enter to leave the fenced code block and continue typing in boring paragraphs or simply use arrow down or press Enter three times.'
-        },
-      ]
+      faqs: ['free', 'code_editor']
     }
   }
 }
@@ -58,6 +36,10 @@ export default {
       "free": {
         "q": "Why is linked free?",
         "a": "Don't worry, you are not paying with your personal data. The app is free and opensource since I like opensource software myself. You will always have full control over your data. If you want to support linked financially, you can consider sponsoring the project."
+      },
+      "code_editor": {
+        "q": "How can I exit the Codeblock",
+        "a": "Press Command/Ctrl + Enter to leave the fenced code block and continue typing in boring paragraphs or simply use arrow down or press Enter three times."
       }
     }
   },
@@ -68,8 +50,12 @@ export default {
     "mailto": "mailto:support@uselinked.com?subject=Frage bzgl. linked&body=Liebes linked Team, könnt ihr mir folgende Frage beantworten....",
     "questions": {
       "free": {
-        "q": "Why is linked free?",
-        "a": "Don't worry, you are not paying with your personal data. The app is free and opensource since I like opensource software myself. You will always have full control over your data. If you want to support linked financially, you can consider sponsoring the project."
+        "q": "Warum ist linked kostenlos?",
+        "a": "Keine Sorge, Du bezahlst nicht mit Deinen persönlichen Daten. Die App ist kostenlos und OpenSource, da ich selbst OpenSource-Software mag. Du wirst immer die volle Kontrolle über Deine Daten haben. Wenn Du linked finanziell unterstützen möchtest, kannst Du das Projekt als Sponsor unterstützen."
+      },
+      "code_editor": {
+        "q": "Wie kann ich den Codeblock verlassen?",
+        "a": "Drücke Befehl/Strg + Enter, um den eingezäunten Codeblock zu verlassen und mit der Eingabe langweiliger Absätze fortzufahren, oder verwende  einfach Pfeil nach unten oder drücke dreimal Enter."
       }
     }
   }

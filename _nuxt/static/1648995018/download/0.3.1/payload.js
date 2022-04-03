7dc7cf3af38a6fc6dc64f7d93df1f7b73f2a4246
lostdesign <lostdesign@users.noreply.github.com>
2022-04-03 14:29:35 +0000

    deploy: 6e9ca0cc584a98ce86d3f63c8054cffd5bbdf70a

--- _nuxt/static/1648995018/download/0.3.1/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/download/0.3.1", {data:[{release:null,error:{error:true,resettingIn:"in 32 minutes",message:"You've exceeded the request count for the Github API, please retry in 32 minutes."},fallbackUrl:"https:\u002F\u002Fgithub.com\u002Flostdesign\u002Flinked\u002Freleases\u002Ftags\u002Fv0.3.1"}],fetch:{},mutations:void 0});

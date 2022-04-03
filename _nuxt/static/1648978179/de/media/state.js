93d4cc775757aeb59a8bda5f0ffdd18ad717343f
lostdesign <lostdesign@users.noreply.github.com>
2022-04-03 14:00:28 +0000

    deploy: b12ff19c4434affc273e4cbda21a91755b67358a

--- _nuxt/static/1648978179/de/media/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1648978179",layout:"default",error:a,serverRendered:true,routePath:"\u002Fde\u002Fmedia",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"507f55d3"}}}}(null));
---
+++ _nuxt/static/1648994421/download/1.1.3/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/download/1.1.3", {data:[{release:null,error:{error:true,resettingIn:"in 59 minutes",message:"You've exceeded the request count for the Github API, please retry in 59 minutes."},fallbackUrl:"https:\u002F\u002Fgithub.com\u002Flostdesign\u002Flinked\u002Freleases\u002Ftags\u002Fv1.1.3"}],fetch:{},mutations:void 0});

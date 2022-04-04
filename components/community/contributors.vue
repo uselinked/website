6b503346721a970e900cadff32250a24dd09a543
wellá <info@lost.design>
2022-04-04 13:06:01 +0200

    ✨ add project maintainer to community

--- components/community/contributors.vue
+++ components/community/contributors.vue
@@ -1,6 +1,26 @@
 <template>
   <div>
     <div v-if="contributors" class="space-y-4 sm:space-y-8 mt-12">
+      <div class="space-y-4 sm:space-y-8">
+        <div class="space-y-5 sm:space-y-4 lg:max-w-5xl">
+          <h2 class="text-3xl font-extrabold tracking-tight sm:text-4xl">{{ $t('maintainer.title') }}</h2>
+          <p class="text-xl text-gray-500">{{ $t('maintainer.subline') }}</p>
+        </div>
+        <a
+          target="_blank"
+          :href="getMaintainer[0].html_url"
+          ref="norel noopener"
+          class="inline-flex flex-col m-0 mb-6 sm:mb-0 sm:m-4 md:m-6 lg:m-8 space-y-4"
+        >
+          <img class="mx-auto h-24 w-24 md:h-20 md:w-20 rounded-full lg:w-24 lg:h-24" :src="getMaintainer[0].avatar_url" :alt="`${getMaintainer[0].login} profile image`" />
+          <div class="space-y-4">
+            <div class="text-center font-medium lg:text-sm">
+              <h3 class="text-base font-bold">{{ getMaintainer[0].login }}</h3>
+              <p class="text-xs text-linked">{{ getMaintainer[0].contributions }} commits</p>
+            </div>
+          </div>
+        </a>
+      </div>
       <div class="space-y-5 sm:space-y-4 lg:max-w-5xl">
         <h2 class="text-3xl font-extrabold tracking-tight sm:text-4xl">{{ $t('lovely_contributors') }}</h2>
         <p class="text-xl text-gray-500">{{ $t('thanks') }}</p>
@@ -42,6 +62,11 @@ export default {
       type: Object,
       default: null,
     }
+  },
+  computed: {
+    getMaintainer() {
+      return this.$props.contributors.filter(contributor => contributor.login === 'lostdesign')
+    }
   }
 }
 </script>
@@ -49,10 +74,18 @@ export default {
 <i18n>
 {
   "en": {
+    "maintainer": {
+      "title": "Maintainer",
+      "subline": "The people behind the project."
+    },
     "lovely_contributors": "Contributors",
     "thanks": "Thanks to the awesome people that have contributed to linked."
   },
   "de": {
+    "maintainer": {
+      "title": "Maintainer",
+      "subline": "Die Menschen hinter dem Projekt."
+    },
     "lovely_contributors": "Mitwirkende",
     "thanks": "Vielen Dank an alle, die zu linked beigetragen haben."
   }

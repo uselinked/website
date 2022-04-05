bd283a8e92da4bdca659883e60a822fb53a277c4
wellá <info@lost.design>
2022-04-05 23:18:45 +0200

    ♻️ modify testimonials, add new one

--- components/section/testimonial.vue
+++ components/section/testimonial.vue
@@ -25,13 +25,25 @@
                 <div class="mt-3 text-center md:mt-0 md:ml-4 md:flex md:items-center space-y-4 md:space-y-0 space-x-0 md:space-x-4">
                   <div class="text-base font-medium text-gray-900">{{ testimonial.name }}</div>
                   <a
-                    v-if="testimonial.twitter"
+                    v-if="testimonial.social"
                     class="inline-block"
-                    :href="testimonial.twitter"
+                    :href="testimonial.social"
                     rel="noreferrer noopener"
                     :aria-label="`Visit ${testimonial.name} on twitter`"
                   >
-                    <icon-twitter class="h-6 w-6 text-blue-400 hover:text-blue-700" />
+                    <component
+                      :is="testimonial.icon"
+                      :class="getIconClasses(testimonial.social)"
+                      class="h-6 w-6"
+                    />
+<!--                    <icon-github-->
+<!--                      v-if="testimonial.icon == 'github'"-->
+<!--                      class="h-6 w-6 text-gray-900 hover:text-gray-700"-->
+<!--                    />-->
+<!--                    <icon-twitter-->
+<!--                      v-if="testimonial.icon == 'twitter'"-->
+<!--                      class="h-6 w-6 text-blue-400 hover:text-blue-700"-->
+<!--                    />-->
                   </a>
                 </div>
               </div>
@@ -46,6 +58,7 @@
 <script>
 import { Glide, GlideSlide } from 'vue-glide-js'
 import IconTwitter from '@/assets/svg/logos/twitter.svg?inline=true'
+import IconGithub from '@/assets/svg/logos/github.svg?inline=true'
 import ArrowLeftIcon from '@/assets/svg/arrow-left.svg?inline=true'
 import ArrowRightIcon from '@/assets/svg/arrow-right.svg?inline=true'
 
@@ -55,27 +68,38 @@ export default {
       testimonials: [
         {
           name: 'Rama Adi Nugraha',
-          twitter: 'https://twitter.com/its_ramaadi',
+          social: 'https://twitter.com/its_ramaadi',
           translationKey: 'rama',
-          image: 'rama.png'
+          image: 'rama.png',
+          icon: IconTwitter
         },
         {
           name: 'Richard',
-          twitter: 'https://twitter.com/richardevcom',
+          social: 'https://twitter.com/richardevcom',
           translationKey: 'richard',
-          image: 'richard.jpeg'
+          image: 'richard.jpeg',
+          icon: IconTwitter
         },
         {
           name: 'Sebastian',
-          twitter: 'https://twitter.com/T0TProduction',
+          social: 'https://github.com/T0TProduction',
           translationKey: 'sebastian',
-          image: 'sebastian.jpeg'
+          image: 'sebastian.jpeg',
+          icon: IconGithub
         },
         {
           name: 'Josiah Rose',
-          twitter: 'https://twitter.com/_josiahrose',
+          social: 'https://twitter.com/_josiahrose',
           translationKey: 'josiah',
-          image: 'josiah.jpeg'
+          image: 'josiah.jpeg',
+          icon: IconTwitter
+        },
+        {
+          name: 'taep96',
+          social: 'https://github.com/taep96',
+          translationKey: 'taep',
+          image: 'taep.jpeg',
+          icon: IconGithub
         }
       ]
     }
@@ -83,12 +107,21 @@ export default {
   computed: {
     slideStart() {
       return Math.floor(Math.random() * this.testimonials.length)
+    },
+  },
+  methods: {
+    getIconClasses(socialUrl) {
+      return [
+        {'text-blue-500 hover:text-blue-700': socialUrl.includes('twitter')},
+        {'text-gray-900 hover:text-gray-700': socialUrl.includes('github')}
+      ]
     }
   },
   components: {
     [Glide.name]: Glide,
     [GlideSlide.name]: GlideSlide,
     IconTwitter,
+    IconGithub,
     ArrowLeftIcon,
     ArrowRightIcon,
   }
@@ -101,13 +134,15 @@ export default {
     "rama": "I love how simple and straightforward linked is. The moment you open the app, you can just start jotting down notes with no fuss. Journaling has never been easier!",
     "richard": "Finally no more countless and forever lost .txt files on my workstation. Linked is perfect solution for everything in between countless todo notes and digital journals.",
     "sebastian": "I use linked at work daily! It's just so convenient for taking quick notes. What I especially like is that everything is stored locally - No need to think about data confidentiality.",
-    "josiah": "I use linked because it helps me effectively manage, track and organize my daily events. It's special because it has a minimalist design while maintaining it's beneficial features and time management."
+    "josiah": "I use linked because it helps me effectively manage, track and organize my daily events. It's special because it has a minimalist design while maintaining it's beneficial features and time management.",
+    "taep": "linked is pog!"
   },
   "de": {
     "rama": "Ich finde es toll, wie einfach und unkompliziert linked ist. In dem Moment, in dem man die App öffnet, kann man ohne viel Aufwand mit dem Notieren beginnen. Journaling war noch nie so einfach!",
     "richard": "Endlich keine unzähligen und für immer verlorenen .txt-Dateien mehr auf meinem Computer. linked ist die perfekte Lösung für alles, was zwischen unzähligen ToDo-Notizen und digitalen Journalen liegt.",
     "sebastian": "Ich verwende linked täglich bei der Arbeit! Es ist einfach so praktisch, um schnelle Notizen zu machen. Besonders gut gefällt mir, dass alles lokal gespeichert wird - man muss sich keine Gedanken über die Vertraulichkeit der Daten machen.",
-    "josiah": "Ich verwende linked, weil es mir hilft, meine täglichen Ereignisse effektiv zu verwalten, zu verfolgen und zu organisieren. Es ist etwas Besonderes, weil es ein minimalistisches Design hat, während es seine nützlichen Funktionen und Zeitmanagement beibehält."
+    "josiah": "Ich verwende linked, weil es mir hilft, meine täglichen Ereignisse effektiv zu verwalten, zu verfolgen und zu organisieren. Es ist etwas Besonderes, weil es ein minimalistisches Design hat, während es seine nützlichen Funktionen und Zeitmanagement beibehält.",
+    "taep": "linked is pog!"
   }
 }
 </i18n>

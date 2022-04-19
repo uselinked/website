ddccaa3c15e3744c08d677debd7a9598cf89654f
lostdesign <lostdesign@users.noreply.github.com>
2022-04-19 07:40:32 +0000

    deploy: 7b8507c03a3f80c0251cb207cb015c190a2ea554

---
+++ _nuxt/static/1650354025/de/changelog/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1650354025",layout:"default",error:a,serverRendered:true,routePath:"\u002Fde\u002Fchangelog",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d2949889"}}}}(null));

74967c9ec823b362b1e50c99d8687f99dbaf4fc2
Sebastian Di Luzio <sebidiluzio@gmail.com>
2022-04-24 17:49:26 +0200

    :sparkles: Count contributions from multiple linked related repos

--- pages/community.vue
+++ pages/community.vue
@@ -6,38 +6,82 @@
 </template>
 
 <script>
-import {checkRateLimit} from '@/lib/github'
+import { checkRateLimit } from "@/lib/github";
+
+async function fetchContributorsOfRepo(orgname, reponame, requestOptions) {
+  return fetch(
+    `https://api.github.com/repos/${orgname}/${reponame}/contributors`,
+    requestOptions
+  );
+}
+
+async function getContributorsOfAllLinkedRepos(githubToken) {
+  const requestOptions = githubToken
+    ? { headers: { Authorization: `Bearer ${githubToken}` } }
+    : {};
+  const responses = [
+    fetchContributorsOfRepo("lostdesign", "linked", requestOptions),
+    fetchContributorsOfRepo("uselinked", "website", requestOptions),
+    fetchContributorsOfRepo("uselinked", "discord", requestOptions),
+  ];
+  const awaitedResponses = await Promise.all(responses);
+  const isRateLimited = awaitedResponses.reduceRight(
+    (alreadyRatelimited, response) =>
+      alreadyRatelimited || checkRateLimit(response),
+    false
+  );
+  if (isRateLimited) {
+    return { isRateLimited, contributors: [] };
+  }
+  const contributorJsons = await Promise.all(
+    awaitedResponses.map((response) => response.json())
+  );
+  const contributorMap = new Map();
+  contributorJsons.forEach((arrayOfContributors) => {
+    arrayOfContributors.forEach((contributor) => {
+      if (contributor.type !== "User") {
+        // don't count bots and potentially other non-user accounts
+        return;
+      }
+      const existingContributor = contributorMap.get(contributor.login);
+      if (existingContributor) {
+        contributorMap.set(contributor.login, {
+          ...contributor,
+          contributions:
+            existingContributor.contributions + contributor.contributions,
+        });
+      } else {
+        contributorMap.set(contributor.login, contributor);
+      }
+    });
+  });
+  return {
+    isRateLimited,
+    contributors: Array.from(contributorMap.values()),
+  };
+}
 
 export default {
-  async asyncData({ $config: { githubToken }}) {
-    let error = null
+  async asyncData({ $config: { githubToken } }) {
+    let error = null;
 
     try {
-      const response = await fetch(
-        'https://api.github.com/repos/lostdesign/linked/contributors',
-        githubToken
-          ? { headers: { Authorization: `Bearer ${githubToken}` } }
-          : {}
-      )
-      console.log(response)
-
-      const isRateLimited = checkRateLimit(response)
-
+      const { isRateLimited, contributors } =
+        await getContributorsOfAllLinkedRepos(githubToken);
       if (isRateLimited.error) {
-        error = isRateLimited
-        throw new Error(isRateLimited.message)
+        error = isRateLimited;
+        throw new Error(isRateLimited.message);
       }
-
       return {
-        contributors: await response.json(),
-        error: null
-      }
+        contributors,
+        error: null,
+      };
     } catch (e) {
       return {
         contributors: null,
         error,
-      }
+      };
     }
-  }
-}
+  },
+};
 </script>

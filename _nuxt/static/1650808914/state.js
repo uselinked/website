8a901bba12bc360414b064b2f43af2f982953a4f
lostdesign <lostdesign@users.noreply.github.com>
2022-04-25 07:28:24 +0000

    deploy: 166ee68e6f59409382b7d06dec53116c9e2697c4

--- _nuxt/static/1650808914/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1650808914",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0b82b037"}}}}(null,"\u002F"));
